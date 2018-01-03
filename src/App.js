ddbbefd480c32951905472a3ae594cc130dd2454
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-01-04 02:14:22 +0800

    #6 Passing Data with Props

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Home from './components/Home';
 
 class App extends Component {
   render() {
+    const user = {
+      name: "Anna",
+      hobbies: ["Sports", "Reading"]
+    }
     return (
       <div className="container">
         <div className="row">
@@ -19,7 +23,9 @@ class App extends Component {
         </div>
         <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
-            <Home />
+            <Home name={"Max"} age={12} user={user}>
+              <p>I am child</p>
+            </Home>
           </div>
         </div>
       </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
 
-export default class Header extends Component {
+import PropTypes from 'prop-types';
+
+export default class Home extends Component {
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
-            <h1>Home</h1>
-            { false ? "hello" : "world" }
+            <div>your name is {this.props.name}, your age is {this.props.age}</div>
+            <div>
+              <h4>hobbies</h4>
+              <ul>
+                {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+              </ul>
+            </div>
+            <div>{this.props.children}</div>
           </div>
         </div>
       </div>
     );
   }
 }
+
+Home.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  user: PropTypes.object,
+  children: PropTypes.element.isRequired
+};
