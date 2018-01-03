afd716045abd920e0ef4bf91034384083541d9c7
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-01-04 00:28:03 +0800

    动态数据

---
+++ src/components/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <h1>Home</h1>
+            { false ? "hello" : "world" }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
