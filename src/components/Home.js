0b221545ac775452034abaa5d7fab0405b190660
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-01-04 11:52:24 +0800

    event

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,19 +3,23 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.age = this.props.age;
+  }
+
+  onMakeOlder() {
+    this.age += 3;
+    console.log(this);
+  }
+
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
             <div>your name is {this.props.name}, your age is {this.props.age}</div>
-            <div>
-              <h4>hobbies</h4>
-              <ul>
-                {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-              </ul>
-            </div>
-            <div>{this.props.children}</div>
+            <button onClick={() => {this.onMakeOlder()}} className="btn btn-primary">Make me older</button>
           </div>
         </div>
       </div>
