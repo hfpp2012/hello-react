2ea7793805b1953a1c4a220b0642ff433ff30b78
hfpp2012 yinsigan <ganweiliang886@gmail.com>
2018-01-04 21:36:33 +0800

    stateless component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,12 +5,21 @@ import PropTypes from 'prop-types';
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.age = this.props.age;
+    this.state = {
+      age: props.initialAge,
+      status: 0
+    }
+    setTimeout(() => {
+      this.setState({
+        status: 1
+      })
+    }, 3000)
   }
 
   onMakeOlder() {
-    this.age += 3;
-    console.log(this);
+    this.setState({
+      age: this.state.age + 3
+    })
   }
 
   render() {
@@ -18,7 +27,8 @@ export default class Home extends Component {
       <div className="container">
         <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
-            <div>your name is {this.props.name}, your age is {this.props.age}</div>
+            <div>your name is {this.props.name}, your age is {this.state.age}</div>
+            <p>Status: {this.state.status}</p>
             <button onClick={() => {this.onMakeOlder()}} className="btn btn-primary">Make me older</button>
           </div>
         </div>
@@ -30,6 +40,5 @@ export default class Home extends Component {
 Home.propTypes = {
   name: PropTypes.string,
   age: PropTypes.number,
-  user: PropTypes.object,
-  children: PropTypes.element.isRequired
+  user: PropTypes.object
 };
